99c4654d8ae090eac7e871b227410b1acd5b66c1
Sonny T <mail@sonnyt.com>
2025-12-17 20:03:22 -0500

    added next.js example

---
+++ examples/nextjs/app/page.tsx
@@ -0,0 +1,9 @@
+import ConnectButton from "./connect";
+
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center p-4">
+      <ConnectButton />
+    </div>
+  );
+}
